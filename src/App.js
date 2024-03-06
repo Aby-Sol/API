ba233a3f5018c83af17640620e66b8035492c5c0
Aby-Sol <Aby.soltani@gmail.Com>
2024-03-06 17:15:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './Card.jsx';
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users").then((result)=>{
+    setUsers(result.data)
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Cards'>
+      <Card/>
     </div>
+   
   );
 }
 
